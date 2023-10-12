ff29a86881558352cb9f7a64bde1ee34261ce477
sologeek <liyoushan3@gmail.com>
2023-10-12 08:18:58 +0800

    feat: 🎸 support msg model

--- src/model.rs
+++ src/model.rs
@@ -1,2 +1,3 @@
 mod bot;
 mod channel;
+mod msg;
---
+++ src/model/msg.rs
@@ -0,0 +1,306 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+/// `Message` is the message struct.
+/// It contains the message role, message active_index and message content list.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Msg {
+  id: Uuid,
+  // role is the sender of the message.
+  role: Role,
+  // `ac_idx` is the index of the current message content show which one.
+  ac_idx: usize,
+  // `cont` is the message content list.
+  cont_list: Vec<MsgCont>,
+  // `meta` is the message meta data.
+  meta: MsgMeta,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct MsgMeta {
+  quote_id: Option<Uuid>,
+  source_id: Option<Uuid>,
+}
+
+impl Msg {
+  pub fn new(role: Role, cont: MsgCont, meta: MsgMeta) -> Self {
+    Self {
+      id: Uuid::new_v4(),
+      role,
+      ac_idx: 0,
+      cont_list: vec![cont],
+      meta,
+    }
+  }
+
+  #[inline]
+  pub fn add_cont(&mut self, cont: MsgCont) {
+    let last_idx = self.cont_list.len();
+    self.cont_list.push(cont);
+    self.ac_idx = last_idx;
+  }
+
+  pub fn switch_cont(&mut self, idx: usize) {
+    let len = self.cont_list.len();
+    if idx >= len {
+      log::warn!(
+        "[polestar] switch message content error: index out of range: message's len is {}, but the index is {}",
+        len,
+        idx
+      );
+      return;
+    }
+    self.ac_idx = idx;
+  }
+
+  #[inline]
+  pub fn ac_idx(&self) -> usize { self.ac_idx }
+
+  #[inline]
+  pub fn role(&self) -> &Role { &self.role }
+
+  #[inline]
+  pub fn cur_cont_as_ref(&self) -> &MsgCont { &self.cont_list[self.ac_idx] }
+
+  #[inline]
+  pub fn cur_cont_as_mut(&mut self) -> &mut MsgCont { &mut self.cont_list[self.ac_idx] }
+
+  #[inline]
+  pub fn cont_count(&self) -> usize { self.cont_list.len() }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Image {
+  width: u32,
+  height: u32,
+  path: ImagePath,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+enum ImagePath {
+  Url(String),
+  File(Uuid),
+  Static(String),
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub enum MsgBody {
+  Text(Option<String>),
+  Image(Option<Image>),
+}
+
+impl MsgBody {
+  pub fn is_some(&self) -> bool {
+    match self {
+      Self::Text(s) => s.is_some(),
+      Self::Image(i) => i.is_some(),
+    }
+  }
+
+  pub fn is_none(&self) -> bool {
+    match self {
+      Self::Text(s) => s.is_none(),
+      Self::Image(i) => i.is_none(),
+    }
+  }
+
+  pub fn set_none(&mut self) {
+    match self {
+      Self::Text(s) => *s = None,
+      Self::Image(i) => *i = None,
+    }
+  }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MsgCont {
+  body: MsgBody,
+  status: MsgStatus,
+}
+
+enum MsgAction {
+  Pending,
+  Fulfilled,
+  Receiving(MsgBody),
+  Rejected,
+}
+
+impl MsgCont {
+  const PENDING_WARN: &'static str =
+    "[polestar] message content action error: pending action must be rejected status";
+  const FULFILLED_WARN: &'static str =
+    "[polestar] message content action error: fulfilled action must be receiving status";
+  const REJECTED_WARN: &'static str =
+    "[polestar] message content action error: rejected action must be pending status";
+  const RECEIVING_WARN: &'static str =
+    "[polestar] message content action error: receiving action must be pending status";
+  pub fn text_init() -> Self {
+    Self {
+      body: MsgBody::Text(None),
+      status: MsgStatus::Pending,
+    }
+  }
+
+  pub fn image_init() -> Self {
+    Self {
+      body: MsgBody::Image(None),
+      status: MsgStatus::Pending,
+    }
+  }
+
+  pub fn action(mut self, action: MsgAction) -> Self {
+    match action {
+      MsgAction::Pending => {
+        if self.status != MsgStatus::Rejected {
+          log::warn!("{}", Self::PENDING_WARN);
+          return self;
+        }
+        self.body.set_none();
+        self.status = MsgStatus::Pending;
+      }
+      MsgAction::Fulfilled => {
+        if self.status != MsgStatus::Receiving {
+          log::warn!("{}", Self::FULFILLED_WARN);
+          return self;
+        }
+        self.status = MsgStatus::Fulfilled;
+      }
+      MsgAction::Receiving(body) => {
+        if self.status != MsgStatus::Pending {
+          log::warn!("{}", Self::RECEIVING_WARN);
+          return self;
+        }
+        self.body = body;
+        self.status = MsgStatus::Receiving;
+      }
+      MsgAction::Rejected => {
+        if self.status != MsgStatus::Pending {
+          log::warn!("{}", Self::REJECTED_WARN);
+          return self;
+        }
+        self.body.set_none();
+        self.status = MsgStatus::Rejected;
+      }
+    };
+    self
+  }
+
+  pub fn body(&self) -> &MsgBody { &self.body }
+
+  pub fn status(&self) -> &MsgStatus { &self.status }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Copy)]
+pub enum MsgStatus {
+  // Send message default status
+  Pending,
+  // Message has response, but not finish
+  Receiving,
+  // Message don't have receive any response and trigger error(i.e timeout, invalid response)
+  Rejected,
+  // Once message has receive response, it will be fulfilled status, and can't update to other
+  // status
+  Fulfilled,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Serialize, Deserialize)]
+pub enum Role {
+  User,
+  Bot(Uuid),
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use log::Level;
+  extern crate testing_logger;
+
+  #[test]
+  fn msg_update() {
+    testing_logger::setup();
+    let mut msg = Msg::new(Role::User, MsgCont::text_init(), MsgMeta::default());
+    msg.add_cont(MsgCont::text_init());
+    assert_eq!(msg.ac_idx(), 1);
+    assert_eq!(msg.cont_count(), 2);
+
+    msg.switch_cont(0);
+    assert_eq!(msg.ac_idx(), 0);
+
+    msg.switch_cont(3);
+    assert_eq!(msg.ac_idx(), 0);
+
+    testing_logger::validate(|captured_logs| {
+      assert_eq!(captured_logs.len(), 1);
+      assert_eq!(
+        captured_logs[0].body,
+        "[polestar] switch message content error: index out of range: message's len is 2, but the index is 3"
+      );
+    });
+  }
+
+  #[test]
+  fn msg_cont_action() {
+    testing_logger::setup();
+
+    let _fulfilled_msg = Msg::new(
+      Role::User,
+      MsgCont::text_init()
+        .action(MsgAction::Receiving(MsgBody::Text(Some("123".to_owned()))))
+        .action(MsgAction::Fulfilled),
+      MsgMeta::default(),
+    );
+
+    let _rejected_msg = Msg::new(
+      Role::User,
+      MsgCont::text_init().action(MsgAction::Rejected),
+      MsgMeta::default(),
+    );
+
+    let _pend_msg = Msg::new(Role::User, MsgCont::text_init(), MsgMeta::default());
+
+    let _wrong_fulfilled_msg = Msg::new(
+      Role::User,
+      MsgCont::text_init().action(MsgAction::Fulfilled),
+      MsgMeta::default(),
+    );
+
+    let _wrong_rejected_msg = Msg::new(
+      Role::User,
+      MsgCont::text_init()
+        .action(MsgAction::Receiving(MsgBody::Text(Some("123".to_owned()))))
+        .action(MsgAction::Rejected),
+      MsgMeta::default(),
+    );
+
+    let _wrong_pend_msg = Msg::new(
+      Role::User,
+      MsgCont::text_init()
+        .action(MsgAction::Receiving(MsgBody::Text(Some("123".to_owned()))))
+        .action(MsgAction::Pending),
+      MsgMeta::default(),
+    );
+
+    let _wrong_receiving_msg = Msg::new(
+      Role::User,
+      MsgCont::text_init()
+        .action(MsgAction::Rejected)
+        .action(MsgAction::Receiving(MsgBody::Text(Some("123".to_owned())))),
+      MsgMeta::default(),
+    );
+
+    testing_logger::validate(|captured_logs| {
+      assert_eq!(captured_logs.len(), 4);
+      assert_eq!(captured_logs[0].body, MsgCont::FULFILLED_WARN,);
+      assert_eq!(captured_logs[0].level, Level::Warn);
+
+      assert_eq!(captured_logs[1].body, MsgCont::REJECTED_WARN,);
+      assert_eq!(captured_logs[1].level, Level::Warn);
+
+      assert_eq!(captured_logs[2].body, MsgCont::PENDING_WARN,);
+      assert_eq!(captured_logs[2].level, Level::Warn);
+
+      assert_eq!(captured_logs[3].body, MsgCont::RECEIVING_WARN,);
+      assert_eq!(captured_logs[3].level, Level::Warn);
+    });
+  }
+}
