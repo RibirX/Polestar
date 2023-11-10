4c09a22145e1e7f732f69f0464482ec60f312357
sologeek <liyoushan3@gmail.com>
2023-11-10 14:59:47 +0800

    feat: 🎸 docs

---
+++ website/README.md
@@ -0,0 +1,1 @@
+# Polestar Website
---
+++ website/docusaurus.config.js
@@ -0,0 +1,92 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Polestar Website',
+  tagline: '',
+  url: 'https://ribirx.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'RibirX',
+  projectName: 'polestar-chat',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', 
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/RibirX/polestar-chat/tree/main/website/',
+          path: '../docs',
+        },
+        blog: false,
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Polestar',
+        logo: {
+          alt: 'Polestar Logo',
+          src: 'img/app_logo.png',
+        },
+        items: [
+          // todo: multiple version config
+          // {
+          //   type: 'docsVersionDropdown',
+          //   position: 'left',
+          //   dropdownActiveClassDisabled: true,
+          // },
+          {
+            type: 'doc',
+            docId: 'get_started',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/RibirX/polestar-chat',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['rust']
+      },
+      metadata: [
+        // TODO: SEO meta data
+        // https://docusaurus.io/docs/2.x/seo#global-metadata
+        { name: 'polestar chat', content: 'polestar chat AI bot' }
+      ]
+    }),
+};
+
+module.exports = config;
