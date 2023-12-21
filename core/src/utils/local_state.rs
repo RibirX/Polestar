8212c21736f7b75726da9e28f8454d541f2e9355
sologeek <liyoushan3@gmail.com>
2023-12-21 16:48:06 +0800

    fix: 🐛 Lost login status when the second open app
    ✅ Closes: #39

--- core/src/utils/local_state.rs
+++ core/src/utils/local_state.rs
@@ -11,31 +11,21 @@ pub struct LocalState {
 }
 
 impl LocalState {
-  // TODO: move app info
-  fn save_local(&self) {
-    if let Some(uid) = self.uid {
-      write_local_state(&uid.to_string(), self).expect("Failed to save local state");
+  pub fn new(
+    cur_channel_id: Option<Uuid>,
+    quick_launcher_id: Option<Uuid>,
+    uid: Option<u64>,
+  ) -> Self {
+    Self {
+      cur_channel_id,
+      quick_launcher_id,
+      uid,
     }
   }
 
   pub fn cur_channel_id(&self) -> &Option<Uuid> { &self.cur_channel_id }
 
-  pub fn set_cur_channel_id(&mut self, cur_channel_id: Option<Uuid>) {
-    self.cur_channel_id = cur_channel_id;
-    self.save_local();
-  }
-
   pub fn quick_launcher_id(&self) -> &Option<Uuid> { &self.quick_launcher_id }
 
-  pub fn set_quick_launcher_id(&mut self, quick_launcher_id: Option<Uuid>) {
-    self.quick_launcher_id = quick_launcher_id;
-    self.save_local();
-  }
-
   pub fn uid(&self) -> &Option<u64> { &self.uid }
-
-  pub fn set_uid(&mut self, uid: Option<u64>) {
-    self.uid = uid;
-    self.save_local();
-  }
 }
